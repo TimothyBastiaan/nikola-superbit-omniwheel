2b0af3f969a82f8535c012dced53141e440c2f3c
TimothyBastiaan <88133865+TimothyBastiaan@users.noreply.github.com>
2024-06-28 13:58:11 +0700

    Update pxt.json, custom.ts, main.py

--- custom.ts
+++ custom.ts
@@ -4,35 +4,75 @@
 * Read more at https://makecode.microbit.org/blocks/custom
 */
 
-enum MyEnum {
-    //% block="one"
-    One,
-    //% block="two"
-    Two
+enum Movement {
+    //% block="Forward"
+    Forward = 1,
+    //% block="Backward"
+    Backward = 2,
+    //% block="Left"
+    Left = 3,
+    //% block="Right"
+    Right = 4,
+    //% block="Crab-Left"
+    CLeft = 5,
+    //% block="Crab-Right"
+    CRight = 6,
+    // Add more protocols as needed
 }
+
 
 /**
  * Custom blocks
  */
-//% weight=100 color=#0fbc11 icon=""
+//% weight=100 color=#0fbc11 icon="\uf085"
 namespace custom {
     /**
-     * TODO: describe your function here
-     * @param n describe parameter here, eg: 5
-     * @param s describe parameter here, eg: "Hello"
-     * @param e describe parameter here
+     * @param command , eg: Movement.Forward
+     * @param rx_pin the new reception pin, eg: SerialPin.P1
      */
-    //% block
-    export function foo(n: number, s: string, e: MyEnum): void {
-        // Add code here
-    }
+    //% blockId="IR create" block="Omniwheel move "
+    //% weight=91 blockGap=8
+    //% parts="nikolaIR_sender"
 
-    /**
-     * TODO: describe your function here
-     * @param value describe value here, eg: 5
-     */
-    //% block
-    export function fib(value: number): number {
-        return value <= 1 ? value : fib(value -1) + fib(value - 2);
+    export function omniwheelMovement(command : Movement, speed: number): void {
+       if(command == Movement.Forward)
+       {
+        SuperBit.MotorRun(SuperBit.enMotors.M1, speed);
+        SuperBit.MotorRun(SuperBit.enMotors.M2, speed);
+        SuperBit.MotorRun(SuperBit.enMotors.M3, speed);
+        SuperBit.MotorRun(SuperBit.enMotors.M4, speed);
+       }
+       else if (command == Movement.Backward) 
+       {
+           SuperBit.MotorRun(SuperBit.enMotors.M1, -1*speed);
+           SuperBit.MotorRun(SuperBit.enMotors.M2, -1*speed);
+           SuperBit.MotorRun(SuperBit.enMotors.M3, -1*speed);
+           SuperBit.MotorRun(SuperBit.enMotors.M4, -1*speed);
+       }
+       else if (command == Movement.Left) {
+           SuperBit.MotorRun(SuperBit.enMotors.M1, speed);
+           SuperBit.MotorRun(SuperBit.enMotors.M2, speed);
+           SuperBit.MotorRun(SuperBit.enMotors.M3, -1*speed);
+           SuperBit.MotorRun(SuperBit.enMotors.M4, -1*speed);
+       }
+       else if (command == Movement.Right) {
+           SuperBit.MotorRun(SuperBit.enMotors.M1, -1*speed);
+           SuperBit.MotorRun(SuperBit.enMotors.M2, -1*speed);
+           SuperBit.MotorRun(SuperBit.enMotors.M3, speed);
+           SuperBit.MotorRun(SuperBit.enMotors.M4, speed);
+       }
+       else if (command == Movement.CLeft) {
+           SuperBit.MotorRun(SuperBit.enMotors.M1, -1*speed);
+           SuperBit.MotorRun(SuperBit.enMotors.M2, speed);
+           SuperBit.MotorRun(SuperBit.enMotors.M3, speed);
+           SuperBit.MotorRun(SuperBit.enMotors.M4, -1*speed);
+       }
+       else if (command == Movement.CRight) {
+           SuperBit.MotorRun(SuperBit.enMotors.M1, speed);
+           SuperBit.MotorRun(SuperBit.enMotors.M2, -1*speed);
+           SuperBit.MotorRun(SuperBit.enMotors.M3, -1*speed);
+           SuperBit.MotorRun(SuperBit.enMotors.M4, speed);
+       }
+       
     }
 }
