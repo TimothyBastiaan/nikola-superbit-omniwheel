6a15c6d5c68d1e7f900b1f6f4aadf105d68f8e57
TimothyBastiaan <88133865+TimothyBastiaan@users.noreply.github.com>
2024-07-02 13:32:47 +0700

    Update nikolaOmniwheel.ts

--- nikolaOmniwheel.ts
+++ nikolaOmniwheel.ts
@@ -66,6 +66,23 @@ namespace NikolaOmniwheel {
             SuperBit.MotorRun(SuperBit.enMotors.M3, -1 * speed);
             SuperBit.MotorRun(SuperBit.enMotors.M4, speed);
         }
+    }
 
+    /**
+     * @param command car movement, eg: Movement.Forward
+     * @param speed speed value, eg: 0
+     */
+    //% blockId="nikola omniwheel individual" block="Omniwheel move m1 %speed1 m2 %speed2 m3 %speed3 m4 %speed4"
+    //% weight=91 blockGap=8
+    //% speed1.min=-255 speed1.max=255
+    //% speed2.min=-255 speed2.max=255
+    //% speed3.min=-255 speed3.max=255
+    //% speed4.min=-255 speed4.max=255
+    //% parts="nikolaOmniwheel"
+    export function individualMovement(speed1: number, speed2: number, speed3: number, speed4: number): void {
+        SuperBit.MotorRun(SuperBit.enMotors.M1, speed1);
+        SuperBit.MotorRun(SuperBit.enMotors.M2, speed2);
+        SuperBit.MotorRun(SuperBit.enMotors.M3, speed3);
+        SuperBit.MotorRun(SuperBit.enMotors.M4, speed4);
     }
 }
